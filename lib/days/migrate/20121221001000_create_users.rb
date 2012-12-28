0de8a3cc5fd2ee05ae96612f0b0866a4ab4f3062
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-12-28 22:03:33 +0900

    Change type of column password_digest on users to binary

--- lib/days/migrate/20121221001000_create_users.rb
+++ lib/days/migrate/20121221001000_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :login_name
-      t.string :password_digest
+      t.binary :password_digest
 
       t.string :name
     end
