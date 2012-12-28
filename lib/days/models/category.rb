93f155c6d857585781b6899377570781ea77e06d
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-12-29 05:24:43 +0900

    Add validation + attr_accessible

--- lib/days/models/category.rb
+++ lib/days/models/category.rb
@@ -2,6 +2,11 @@ require 'active_record'
 
 module Days
   class Category < ActiveRecord::Base
+    attr_accessible :name
+
+    validates_presence_of :name
+    validates_uniqueness_of :name
+
     has_and_belongs_to_many :entries, class_name: 'Days::Entry'
   end
 end
--- lib/days/models/user.rb
+++ lib/days/models/user.rb
@@ -4,6 +4,11 @@ module Days
   class User < ActiveRecord::Base
     has_secure_password
 
+    attr_accessible :login_name, :name, :password, :password_confirmation
+
+    validates_uniqueness_of :login_name
+    validates_presence_of :login_name, :name
+
     has_many :entries, class_name: 'Days::Entry'
   end
 end
