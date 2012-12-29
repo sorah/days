fb7c8da97d1e477a85b8497df86a327b22b4291a
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-12-30 03:58:20 +0900

    Make /admin/ as admin only
    * /admin for redirection

--- lib/days/app/admin.rb
+++ lib/days/app/admin.rb
@@ -1,6 +1,10 @@
 module Days
   class App < Sinatra::Base
     get "/admin" do
+      redirect "/admin/"
+    end
+
+    get "/admin/", admin_only: true do
       haml :'admin/index', layout: :admin
     end
   end
