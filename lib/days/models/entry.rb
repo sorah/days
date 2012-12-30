041c8035fa52107f19d01af265f1805b3fdd446b
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-12-30 23:48:02 +0900

    Add draft attribute

--- lib/days/models/entry.rb
+++ lib/days/models/entry.rb
@@ -4,7 +4,7 @@ require 'redcarpet'
 
 module Days
   class Entry < ActiveRecord::Base
-    attr_accessible :title, :body, :slug, :published_at, :categories, :user
+    attr_accessible :title, :body, :slug, :published_at, :categories, :user, :draft
 
     validates_uniqueness_of :slug
     validates_presence_of :title, :body, :rendered, :slug
@@ -12,8 +12,40 @@ module Days
     belongs_to :user, class_name: 'Days::User'
     has_and_belongs_to_many :categories, class_name: 'Days::Category'
 
+    scope :published, -> do
+      where('published_at IS NOT NULL AND published_at <= ?', Time.now).
+      order('published_at DESC')
+    end
+
+    def draft=(x)
+      if x.present?
+        @draft = true
+      else
+        @draft = false
+      end
+    end
+
+    def draft?
+      @draft
+    end
+    alias draft draft?
+
+    def published?
+      self.published_at && self.published_at <= Time.now
+    end
+
+    def scheduled?
+      self.published_at && Time.now < self.published_at
+    end
+
     before_validation do
-      if self.title && !self.slug
+      if draft?
+        self.published_at = nil
+      else
+        self.published_at ||= Time.now
+      end
+
+      if self.title && (!self.slug || self.slug.empty?)
         self.slug = self.title.to_url
       end
       markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -39,8 +39,170 @@ describe Days::Entry do
 
     it { should be_valid }
 
-    specify do
+    it "generates slug"do
       subject.slug.should == 'slug'
+    end
+
+    context "with empty slug" do
+      before do
+        String.any_instance.stub(to_url: "slug")
+        subject.slug = ""
+        subject.save
+      end
+
+      it { should be_valid }
+
+      it "generates slug"do
+        subject.slug.should == 'slug'
+      end
+    end
+  end
+
+  describe "#published?" do
+    context "with nullified published_at" do
+      before do
+        subject.published_at = nil
+      end
+
+      specify do
+        subject.should_not be_published
+      end
+
+      specify do
+        subject.should_not be_scheduled
+      end
+    end
+
+    context "when after published_at" do
+      before do
+        base = Time.now
+        Time.stub(now: base)
+        subject.published_at = base - 1
+      end
+
+      specify do
+        subject.should be_published
+      end
+
+      specify do
+        subject.should_not be_scheduled
+      end
+    end
+
+    context "when just published_at" do
+      before do
+        base = Time.now
+        Time.stub(now: base)
+        subject.published_at = base
+      end
+
+      specify do
+        subject.should be_published
+      end
+
+      specify do
+        subject.should_not be_scheduled
+      end
+    end
+
+    context "when before published_at" do
+      before do
+        base = Time.now
+        Time.stub(now: base)
+        subject.published_at = base + 1
+      end
+
+      specify do
+        subject.should_not be_published
+      end
+
+      specify do
+        subject.should be_scheduled
+      end
+    end
+  end
+
+  describe "published scope" do
+    before do
+      described_class.create(title: 'A', body: 'a', draft: true)
+      described_class.create(title: 'B', body: 'b', published_at: Time.local(2012,12,30,11,0,0))
+      described_class.create(title: 'C', body: 'c', published_at: Time.local(2012,12,30, 9,0,0))
+      described_class.create(title: 'D', body: 'd', published_at: Time.local(2013, 1, 1, 9,0,0))
+      Time.stub(now: Time.local(2012,12,30, 14,0,0))
+    end
+
+    subject do
+      described_class.published
+    end
+
+    it "orders by published_at" do
+      subject.map(&:title).should == ['B', 'C']
+    end
+
+    it "rejects not published entries" do
+      subject.map(&:title).should_not include('A')
+      subject.map(&:title).should_not include('D')
+    end
+
+    context "all published" do
+      before do
+        Time.stub(now: Time.local(2013,1,1,14,0,0))
+      end
+
+      it "orders by published_at" do
+        subject.map(&:title).should == ['D', 'B', 'C']
+      end
+
+      it "rejects not published entries" do
+        subject.map(&:title).should_not include('A')
+        subject.map(&:title).should include('D')
+      end
+    end
+  end
+
+  describe "draft attribute" do
+    context "with true" do
+      before do
+        subject.published_at = Time.now
+        subject.draft = true
+        subject.valid?
+      end
+
+      it "nullifies published_at" do
+        subject.published_at.should be_nil
+      end
+    end
+
+    context "with false" do
+      let!(:base) { Time.now }
+
+      before do
+        subject.draft = false
+      end
+
+      context "when published_at is nil" do
+        before do
+          Time.stub(now: base)
+          subject.published_at = nil
+          subject.valid?
+        end
+
+        it "fills published_at" do
+          subject.published_at.should == base
+        end
+      end
+
+      context "when published_at is not nil" do
+        before do
+          Time.stub(now: base + 1)
+          subject.published_at = base
+          subject.valid?
+        end
+
+        it "keeps published_at" do
+          subject.published_at.should == base
+        end
+      end
     end
   end
 end
