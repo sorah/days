0cbb58d58adccda7eaaf435d14af6926b6ce8c37
Shota Fukumori (sora_h) <her@sorah.jp>
2015-01-09 01:50:20 +0900

    give Sequel::Model an dummy DB
    Sequel::Model requires active database connection on class inheritance.
    What?? https://groups.google.com/forum/#!topic/sequel-talk/0x_AyzIIGUo
    
    It is hard to controll order of class loading, so we do workaround by
    this.

--- lib/days/models/base.rb
+++ lib/days/models/base.rb
@@ -2,7 +2,13 @@ require 'sequel'
 
 module Days
   module Models
-    class Base < Sequel::Model
+    # Sequel::Model requires active database connection on class inheritance.
+    # What?? https://groups.google.com/forum/#!topic/sequel-talk/0x_AyzIIGUo
+    def self.dummy_connection
+      # on memory
+      @dummy_connection ||= Sequel.sqlite
+    end
+    class Base < Sequel::Model(self.dummy_connection[:base])
     end
   end
 end
